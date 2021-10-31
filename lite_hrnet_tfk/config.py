cda4b683f2f966861dc1a3fe3db458496e0b61bf
Boris Zimka <zimka.b@ya.ru>
2021-10-31 14:23:05 +0300

    Update functional api

--- lite_hrnet_tfk/config.py
+++ lite_hrnet_tfk/config.py
@@ -16,27 +16,37 @@ class StemSpec:
     out_channels: int = 32
     expand_ration: int = 1
 
+@dataclass
+class HeadSpec:
+    _HEAD_VERSIONS = (
+        'v0', # None, just forward all scales
+        'v1', # Forward scale with the highest resolution
+        'v2', # Upsample, concat and apply conv 1x1
+        'v2p'
+    )
+    v2_out_channels: int = 32
+    version: str = 'v2'
+    v1_scale_idx: int = 0
+
+
 
 @dataclass
 class LiteHrnetConfig:
-    stage_specs: List[StageSpec]
-    stem_spec: StemSpec
-
-    head_output_channels: int
-    head_version: str = 'v2'
-    _HEAD_VERSIONS = ('v1', 'v2', 'v2p')
+    stages: List[StageSpec]
+    stem: StemSpec
+    head: HeadSpec
 
     name: str = "LiteHrNet"
 
     @classmethod
     def naive18(cls, head_output_channels=32):
         return cls(
-            stage_specs=[
+            stages=[
                 StageSpec(num_modules=2, num_blocks=2, num_channels_list=(40, 80)),
                 StageSpec(num_modules=4, num_blocks=2, num_channels_list=(40, 80, 160)),
                 StageSpec(num_modules=4, num_blocks=2, num_channels_list=(40, 80, 160, 320)),
             ],
-            stem_spec=StemSpec(),
+            stem=StemSpec(),
             name="LiteHrNetNaive18",
-            head_output_channels=head_output_channels
+            head=HeadSpec()
         )
--- lite_hrnet_tfk/net.py
+++ lite_hrnet_tfk/net.py
@@ -4,17 +4,18 @@ from lite_hrnet_tfk.modules import StemModule, LiteNaiveHrModule, TransitionModu
 from lite_hrnet_tfk.config import LiteHrnetConfig
 
 
-def build_func_api_net(x, config:LiteHrnetConfig):
+def lite_hr_net(x=tf.keras.layers.Input((256, 256, 3)), *, config:LiteHrnetConfig, return_model=True):
     stem_m = StemModule(
-        stem_channels=config.stem_spec.stem_channels,
-        out_channels=config.stem_spec.out_channels,
+        stem_channels=config.stem.stem_channels,
+        out_channels=config.stem.out_channels,
         name=f"{config.name}.stem"
     )
+    inputs = x
     x = stem_m(x)
     x = [x]
-    branches_chan_list_before = [config.stem_spec.out_channels]
+    branches_chan_list_before = [config.stem.out_channels]
     branches_chan_list_after = []
-    for spec_idx, spec in enumerate(config.stage_specs):
+    for spec_idx, spec in enumerate(config.stages):
         branches_chan_list_after = spec.num_channels_list
         trans_m = TransitionModule(
             branches_chan_list_dst=branches_chan_list_after,
@@ -30,27 +31,35 @@ def build_func_api_net(x, config:LiteHrnetConfig):
             x = lhr_m(x)
         branches_chan_list_before = branches_chan_list_after
 
-    assert config.head_version in LiteHrnetConfig._HEAD_VERSIONS
-    if config.head_version != 'v2':
+    assert config.head.version in config.head._HEAD_VERSIONS
+    outputs = None
+    if config.head.version == 'v0':
+        outputs = x
+    elif config.head.version == 'v1':
+        outputs = x[config.head.v1_scale_idx]
+    elif config.head.version == 'v2':
+        head = HrNetHeadV2(num_scales=len(config.stages), output_channels=config.head.v2_out_channels, name=f"{config.name}.head")
+        outputs = head(x)
+    else:
         raise NotImplementedError()
-
-    head = HrNetHeadV2(num_scales=len(config.stage_specs), output_channels=config.head_output_channels, name=f"{config.name}.head")
-    x = head(x)
-    return x
+    if return_model:
+        return tf.keras.models.Model(inputs=inputs, outputs=outputs)
+    else:
+        return (inputs, outputs)
 
 
 class LiteHrNet(tf.keras.models.Model):
     def __init__(self, config: LiteHrnetConfig):
         super().__init__()
         self.stem_m = StemModule(
-            stem_channels=config.stem_spec.stem_channels,
-            out_channels=config.stem_spec.out_channels,
+            stem_channels=config.stem.stem_channels,
+            out_channels=config.stem.out_channels,
             name=f"{config.name}.stem"
         )
-        branches_chan_list_before = [config.stem_spec.out_channels]
+        branches_chan_list_before = [config.stem.out_channels]
         branches_chan_list_after = []
         self.stages = []
-        for spec_idx, spec in enumerate(config.stage_specs):
+        for spec_idx, spec in enumerate(config.stages):
             stage_modules = []
             branches_chan_list_after = spec.num_channels_list
             stage_modules.append(
@@ -72,11 +81,18 @@ class LiteHrNet(tf.keras.models.Model):
             branches_chan_list_before = branches_chan_list_after
             self.stages.append(stage_modules)
 
-        assert config.head_version in LiteHrnetConfig._HEAD_VERSIONS
-        if config.head_version != 'v2':
+        assert config.head.version in config.head._HEAD_VERSIONS
+        if config.head.version == 'v0':
+            self.head = lambda x : x
+        elif config.head.version == 'v1':
+             self.head = lambda x : x[config.head.v1_scale_idx]
+        if config.head.version != 'v2':
             raise NotImplementedError()
 
-        self.head = HrNetHeadV2(num_scales=len(config.stage_specs), output_channels=config.head_output_channels, name=f"{config.name}.head")
+        head = HrNetHeadV2(num_scales=len(config.stages), output_channels=config.head.v2_out_channels, name=f"{config.name}.head")
+        if config.head.version != 'v2':
+            raise NotImplementedError()
+        self.head = HrNetHeadV2(num_scales=len(config.stages), output_channels=config.head_output_channels, name=f"{config.name}.head")
 
     def call(self, x):
         x = self.stem_m(x)
